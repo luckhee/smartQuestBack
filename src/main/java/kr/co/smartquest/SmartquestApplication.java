1b3784b588f1fb4144b7eec51ad9bbc1aa9ea7c8
luckhee <114234435+luckhee@users.noreply.github.com>
2024-11-29 20:42:34 +0900

    first commit

---
+++ src/main/java/kr/co/smartquest/SmartquestApplication.java
@@ -0,0 +1,13 @@
+package kr.co.smartquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartquestApplication.class, args);
+	}
+
+}
