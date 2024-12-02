0017bb7fa53f94686ad66ab9213d752e907a48af
luckhee <114234435+luckhee@users.noreply.github.com>
2024-12-02 15:39:31 +0900

    API추가

--- src/main/java/kr/co/smartquest/SmartquestApplication.java
+++ src/main/java/kr/co/smartquest/SmartquestApplication.java
@@ -1,13 +1,25 @@
 package kr.co.smartquest;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @SpringBootApplication
 public class SmartquestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmartquestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SmartquestApplication.class, args);
+    }
 
+    @Bean
+    public ApplicationRunner runner(DataSource dataSource) {
+        return args -> {
+
+            Connection connection = dataSource.getConnection();
+        };
+    }
 }
--- src/test/java/kr/co/smartquest/SmartquestApplicationTests.java
+++ src/test/java/kr/co/smartquest/SmartquestApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SmartquestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
