17b37c5ad08a2cdda6d03d4fca72a9e8abec9048
luckhee <114234435+luckhee@users.noreply.github.com>
2024-12-02 20:10:20 +0900

    API추가

--- src/main/java/kr/co/smartquest/SmartquestApplication.java
+++ src/main/java/kr/co/smartquest/SmartquestApplication.java
@@ -1,5 +1,7 @@
 package kr.co.smartquest;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,5 +23,14 @@ public class SmartquestApplication {
 
             Connection connection = dataSource.getConnection();
         };
+    }
+
+    @Bean
+    public ModelMapper modelMapper() { // 하는 이유가 setter 없이 product랑 prodcutDto 변환 해주려고
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().
+                setFieldAccessLevel(Configuration.AccessLevel.PRIVATE).
+                setFieldMatchingEnabled(true);
+        return modelMapper;
     }
 }
