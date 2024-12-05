cc8651e02250c8284dfbd8d9ee20bd744ab7f662
luckhee <114234435+luckhee@users.noreply.github.com>
2024-12-05 22:14:45 +0900

    Add files via upload

---
+++ EnterUserService.java
@@ -0,0 +1,46 @@
+package kr.co.smartquest.application;
+
+import kr.co.smartquest.domain.User;
+import kr.co.smartquest.infrastructure.UserRepository;
+import kr.co.smartquest.presentation.CreateUser.CreateUserRequestDto;
+import kr.co.smartquest.presentation.CreateUser.CreateUserResponseDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EnterUserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+
+    public CreateUserResponseDto saveUser(CreateUserRequestDto createUserRequestDto) {
+        String encodedPassword = passwordEncoder.encode(createUserRequestDto.getPassword());
+
+        User user = new User(
+                createUserRequestDto.getId(),
+                createUserRequestDto.getName(),
+                encodedPassword
+        );
+
+        userRepository.usersave(user);
+
+        return new CreateUserResponseDto(user);
+//        String id = createUserRequestDto.getId();
+//        String name = createUserRequestDto.getName();
+//        String password = createUserRequestDto.getPassword();
+//
+//        User user = new User(id, name, password);
+//
+//        userRepository.usersave(user);
+//
+//        CreateUserResponseDto createUserResponseDto = new CreateUserResponseDto(user);
+//
+//        return createUserResponseDto;
+    }
+}
---
+++ QuestService.java
@@ -0,0 +1,45 @@
+package kr.co.smartquest.application;
+
+import kr.co.smartquest.domain.Quest;
+
+import kr.co.smartquest.infrastructure.QuestRepository;
+import kr.co.smartquest.presentation.Quest.CreateQuestDto;
+import kr.co.smartquest.presentation.Quest.QuestDto;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class QuestService {
+
+    private QuestRepository questRepository;
+    private ModelMapper modelMapper;
+
+
+    @Autowired
+    public QuestService(QuestRepository questRepository, ModelMapper modelMapper) {
+        this.questRepository = questRepository;
+        this.modelMapper = modelMapper;
+
+    }
+
+
+
+
+    public void addQuest(CreateQuestDto createQuestDto) {
+        Quest quest = new Quest(createQuestDto);
+        questRepository.questSave(quest);
+    }
+
+    public List<QuestDto> findAllQuest() {
+        List<Quest> quest = questRepository.findAllQuest();
+        List<QuestDto> questDto = quest.stream()
+                .map(quests -> modelMapper.map(quests, QuestDto.class))
+                .toList();
+
+        return questDto;
+    }
+}
