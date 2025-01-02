568422d3eaeafb2049e598e9c9f3135390ea2955
luckhee <114234435+luckhee@users.noreply.github.com>
2025-01-02 14:39:36 +0900

    Add files via upload
    Response 수정

--- src/main/java/kr/co/smartquest/controller/SmartQuestController.java
+++ src/main/java/kr/co/smartquest/controller/SmartQuestController.java
@@ -1,10 +1,9 @@
-package kr.co.smartquest.controller;
+package kr.co.smartquest.presentation.controller;
 
-import kr.co.smartquest.application.EnterUserService;
-import kr.co.smartquest.application.LoginService;
-import kr.co.smartquest.application.QuestService;
+import kr.co.smartquest.application.Service.EnterUserService;
+import kr.co.smartquest.application.Service.LoginService;
+import kr.co.smartquest.application.Service.QuestService;
 import kr.co.smartquest.presentation.CreateUser.CreateUserRequestDto;
-import kr.co.smartquest.presentation.CreateUser.CreateUserResponseDto;
 import kr.co.smartquest.presentation.Login.TryLoginDto;
 import kr.co.smartquest.presentation.Quest.CreateQuestDto;
 import kr.co.smartquest.presentation.Quest.QuestDto;
@@ -12,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.springframework.http.ResponseEntity.ok;
 
@@ -50,10 +51,11 @@ public class SmartQuestController {
     //로그인 API result가 true면 리턴타입을 ResponseEntity<BatchQuest>로 간다던지
     @RequestMapping(value = "/smartquestLogin", method = RequestMethod.POST)
     public ResponseEntity<?> login(@RequestBody TryLoginDto tryLoginDto) {
-        String email = tryLoginDto.getId();
+        String email = tryLoginDto.getEmail();
         loginService.loadUserByUsername(email);
-
-        return ResponseEntity.ok("회원가입 완료");
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "로그인 완료");
+        return ResponseEntity.ok(response);
     }
 
     //퀘스트 추가 API 완료.
